6af3230e13374b540ec3d29807843ecbfaa9089a
Willian <williamhernandez450@gmail.com>
2023-12-14 07:18:22 -0600

    Config conection to data base and create entity Employee

---
+++ src/main/java/com/whernandez/cruddemo/entity/Employee.java
@@ -0,0 +1,18 @@
+package com.whernandez.cruddemo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="employee")
+public class Employee {
+
+	//define fields
+	
+	// define constructors
+	
+	// define getter/setter
+	
+	// define toString
+	
+}
