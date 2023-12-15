1d8e1a0f9647e6368b6cfbee89156785ffe767a9
Willian <williamhernandez450@gmail.com>
2023-12-14 19:59:56 -0600

    Implements methos in service

--- src/main/java/com/whernandez/cruddemo/service/EmployeeService.java
+++ src/main/java/com/whernandez/cruddemo/service/EmployeeService.java
@@ -7,5 +7,11 @@ import com.whernandez.cruddemo.entity.Employee;
 public interface EmployeeService {
 	
 	List<Employee> findAll();
+	
+	Employee findById(int theId);
+	
+	Employee save(Employee theEmployee);
+	
+	void deleteById(int theId);
 
 }
